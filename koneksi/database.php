75fea6ca42ac488f3026d6894853f71b91d6afe4
ARMH1180YT <97026873+ARMH1180YT@users.noreply.github.com>
2026-09-01 10:17:22 +0700

    Fix PDO error mode setting in database connection

--- koneksi/database.php
+++ koneksi/database.php
@@ -10,7 +10,7 @@ try {
 
      $connection = new PDO("mysql:host=$host:$port;dbname=$database", $username, $password);
      echo "sukses terkoneksi ke satabase" . "\n";
-     $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::ERRMODE_EXCEPTION);
+     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
      return $connection;
      } catch (PDOException $exception){
          echo "error terkoneksi ke database :" . $exception->getMessage() . PHP_EOL;
